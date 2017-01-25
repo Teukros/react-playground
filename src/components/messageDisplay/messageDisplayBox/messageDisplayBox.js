95619e84b749ddb74724c941daaf4dc0352ec5e3
Bolec <subotkowski.adam@vrglobal.com>
2017-01-25 14:56:24 +0100

    added material

--- src/components/messageDisplay/messageDisplayBox/messageDisplayBox.js
+++ src/components/messageDisplay/messageDisplayBox/messageDisplayBox.js
@@ -1,18 +1,24 @@
 'use strict';
+import muiThemeable from 'material-ui/styles/muiThemeable';
+import RaisedButton from 'material-ui/RaisedButton';
+
 
 const React = require('react'),
   MessageDisplayText = require('../messageDisplayText/messageDisplayText');
 
 class messageDisplayBox extends React.Component{
 
+constructor(props){
+  super()
+}
   render(){
 
     return (
       <div>Here are messages from messageDisplayBox
+        <RaisedButton label="Default" />
       <MessageDisplayText posts = {this.props.posts}/>
     </div>);
-
   }
 }
 
-module.exports = messageDisplayBox;
+module.exports = muiThemeable()(messageDisplayBox);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 'use strict';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 
 const React = require('react');
 const ReactDOM = require('react-dom');
 const AppComponent = require('components/app/app');
 const storage = require('./helpers/storage');
 
+storage.addPost("This is some random post!")
+
 const container = document.querySelector('.app');
 const posts = storage.getPosts();
 
-ReactDOM.render(
-  <AppComponent posts = {posts}/>,
-  container
+const App = () => (
+  <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+    <AppComponent posts = {posts}/>
+  </MuiThemeProvider>
 );
+
+ReactDOM.render(
+  <App />,
+  container
+    );
+
+
