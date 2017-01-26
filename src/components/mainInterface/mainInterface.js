0f7b3dba4c5eaac8839f88d53344b809b7af8d59
tomlos <tomasz.los.iu@gmail.com>
2017-01-26 13:03:29 +0100

    appbar added

--- src/components/mainInterface/mainInterface.js
+++ src/components/mainInterface/mainInterface.js
@@ -1,9 +1,13 @@
 'use strict';
+import AppBar from 'material-ui/AppBar';
+import {Tabs, Tab} from 'material-ui/Tabs';
+
 
 const React = require('react'),
   storage = require('../../helpers/storage'),
   MessageDisplayBox = require('../messageDisplay/messageDisplayBox/messageDisplayBox'),
   MessageEditor = require('../messageEditor/messageEditor');
+
 
 
 class MainInterface extends React.Component{
@@ -13,9 +17,17 @@ class MainInterface extends React.Component{
     const posts = storage.getPosts();
 
     return (
-      <div className="panel panel-default"> Below you can find a components of Main Interface:
+      <div className="panel panel-default">
         <div>
-          <MessageEditor/>
+        <AppBar
+  title=" Below you can find a components of Main Interface:"
+/>
+<Tabs>
+<Tab label=" Below you can find a components of Main Interface:"  >
+
+  </Tab>
+  </Tabs>
+  <MessageEditor/>
         </div>
         <div>Below you can find a list of messages.
           <MessageDisplayBox posts = {posts}/>
