6d6bad0bc7712079cab26c3223e6b1815c2e7243
tomlos <tomasz.los.iu@gmail.com>
2017-01-27 15:26:44 +0100

    working send button on form

--- src/components/messageEditor/messageEditor.js
+++ src/components/messageEditor/messageEditor.js
@@ -46,39 +46,44 @@ const buttonDivHolder = {
   justifyContent: "center"
 }
 
+
 class messageEditor extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: 'enter something here'
+
     };
     this.handleChange = this.handleChange.bind(this);
   };
 
-  getValidationState() {
-    const length = this.state.value.length;
-    if (length > 10)
-      return 'success';
-    else if (length > 5)
-      return 'warning';
-    else if (length > 0)
-      return 'error';
-    };
+  // getValidationState() {
+  //   const length = this.state.value.length;
+  //   if (length > 10)
+  //     return 'success';
+  //   else if (length > 5)
+  //     return 'warning';
+  //   else if (length > 0)
+  //     return 'error';
+  //   };
 
   handleChange(e) {
 
     this.setState({value: e.target.value});
+    console.log('ustawiam stan')
+
   };
 
   render() {
     return (
       <div>
         <div style={divForInputStyle}>
-          <TextField id="text-field-controlled" value={this.state.value} onChange={this.handleChange} id="formBasicText" defaultValue="dupa" inputStyle={inputStyle} textareaStyle={inputStyle} multiLine="true" style={mainAreaInputStyle} underlineShow="true" underlineStyle={underlineStyle} floatingLabelFixed="true" floatingLabelText="Working example with validation" floatingLabelStyle={floatingLabelStyle}/>
+          <TextField id="text-field-controlled" value={this.state.value} onChange={this.handleChange} hintText="Place for your suggestions" id="formBasicText" inputStyle={inputStyle} textareaStyle={inputStyle} multiLine="true" style={mainAreaInputStyle}  underlineShow="true" underlineStyle={underlineStyle} floatingLabelFixed="true" floatingLabelText="Working example with validation" floatingLabelStyle={floatingLabelStyle}   />
 
         </div>
         <div style={buttonDivHolder}>
-          <RaisedButton label="Wyślij" primary={true} style={sendButtonStyle} labelStyle={sendButtonStyle}/>
+          <RaisedButton label="Wyślij" on  primary={true} style={sendButtonStyle} labelStyle={sendButtonStyle}
+                  onTouchTap= {storage.addPost(this.state.value) }
+           />
         </div>
       </div>
 
